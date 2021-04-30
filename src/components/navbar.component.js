a86272c3c96cc60d8d6543a5fe3d82dce57967a9
lmarqu8 <lmarqu8@uic.edu>
2021-04-30 02:45:55 -0500

    navbar test

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
                 <Link to="/" className="navbar-brand">Bruxish.DB</Link>
                 <div className="collapse navbar-collapse">
                 <ul className="navbar-nav mr-auto">
